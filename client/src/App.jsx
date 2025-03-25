db7da0b7e5fb6c880532eeb3daebff068a187c9c
sudesh-web <sudeshdhodi11203@gmail.com>
2025-03-25 11:36:07 +0530

    create and add the footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Projects from './pages/Projects'
 import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 
 
@@ -22,6 +23,7 @@ export default function App() {
         <Route path="/projects" element={<Projects />} />
         
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
