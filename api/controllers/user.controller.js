5918dc152ce10a344a77f388347d7ec40ae2aacf
sudesh-web <sudeshdhodi11203@gmail.com>
2025-04-07 00:28:35 +0530

    add delete user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -48,6 +48,17 @@ if (req.body.username){
     } catch (error) {
         next(error);
     }
- 
 
+};
+
+export const deleteUser = async (req, res, next) => {
+   if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'You are not allowed to delete this user'));
+   } 
+   try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json('User has been dleted');
+   } catch (error) {
+    next(error);
+   }
 };
